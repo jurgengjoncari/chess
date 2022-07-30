67a4c9f624993ed2347c4c8f64a2806f0d4ab85e
Jurgen Gjonçari <jurgen_gjoncari@icloud.com>
2022-07-30 21:34:07 +0200

    Add the checkerboard

---
+++ index.js
@@ -0,0 +1,14 @@
+let container = document.querySelector('[data-board]')
+
+let board = [...Array(8)].map(e => Array(8))
+
+console.log(board);
+
+for (let i = 0; i < 8; i++) {
+    let board = []
+    let rank = i
+    for (let j = 0; j < 8; j++) {
+        let file = j
+        
+    }
+}
